f84d7a536bef4e397f6dc45bccddc83f40349a72
toxidity-18 <desavigatunganga@gmail.com>
2024-09-08 15:27:26 +0300

    first

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
+    with open(f'{file_name}.txt', 'w') as f:
+        f.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    with open(f'{file_name}.txt', 'a') as f:
+      f.write(append_content)
+
     pass
 
+
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', 'r') as f:
+        file_content_read = f.read()
+    return file_content_read
+pass
